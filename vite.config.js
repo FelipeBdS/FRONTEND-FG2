ea2f1bcbf62c6b5c2d73bb903d405dbaec180012
FelipeBdS <felipe-gdp1@hotmail.com>
2023-11-27 18:00:18 -0300

    App.jsx changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/FRONTEND-FG2/"
-})
+  base: "/FRONTEND-FG2/",
+  build: {
+    rollupOptions: {
+      external: [
+        // Adicione os caminhos dos módulos que você deseja externalizar
+        "/FRONTEND-FG2/assets/index-lNpJ9YsO.js",
+      ],
+    },
+  },
+});
